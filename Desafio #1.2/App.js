fa63b5dd9810242cbe4c2d00b0bc97065b7438b1
Ferdzen <fernandaivp63@gmail.com>
2023-07-10 23:57:40 -0300

    App.js criado; testes de aplicação realizados.

--- Desafio #1.2/App.js
+++ Desafio #1.2/App.js
@@ -1,7 +1,7 @@
+import ControllerMenus from './src/controller/ControllerMenus.js';
 import PromptSync from 'prompt-sync';
 const prompt = PromptSync({ sigint: true }); // Entrada de dados
 
+const cm = new ControllerMenus();
 
-let nome = prompt("Whats your name? ");
-
-console.log(`Name: ${nome}`)
+cm.iniciarMenu();
