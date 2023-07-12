14460fca7df68a085769a18648d894149fe29424
Ferdzen <fernandaivp63@gmail.com>
2023-07-12 19:16:00 -0300

    Correções e implementação dos métodos listas (por nome e cpf) e delete

--- Desafio #1.2/src/controller/ControllerPaciente.js
+++ Desafio #1.2/src/controller/ControllerPaciente.js
@@ -2,25 +2,25 @@ import MenuCadastro from "../view/MenuCadastro.js";
 import Paciente from "../model/Paciente.js"
 import Table from 'cli-table'; //impressão de listagens
 
-export default class ControllerPaciente{
+export default class ControllerPaciente {
 
     //O objetivo desta classe é receber os dados do usuário e fazer a primeira leva de tratamento de dados, que consiste na verificação se o input foi feito corretamente e conversão para posteriores validações na classe Paciente.
-    constructor(){
+    constructor() {
         this.viewCadastro = new MenuCadastro();
         this.modelPaciente = new Paciente();
     }
 
-    iniciaCadastro(){
+    iniciaCadastro() {
         let nome = this.viewCadastro.recebeNome();
         let cpf = this.viewCadastro.recebeCpf();
         let dataNasc = this.viewCadastro.recebeDataNascimento();
 
         //Verificando se inputs foram corretos
-        if(this.validaNome(nome) == true && (this.qtdCaracteresCPF(cpf) == true && this.verificaDigitosCPF(cpf) == false)){
+        if (this.validaNome(nome) == true && (this.qtdCaracteresCPF(cpf) == true && this.verificaDigitosCPF(cpf) == false)) {
             //Validação de dados
-            if(this.modelPaciente.validaDvsCpf(cpf) == true && 
-            this.modelPaciente.verificaIdade(dataNasc[0], dataNasc[1], dataNasc[2]) >= 13){
-                if(this.modelPaciente.verificaRegistro(this.modelPaciente.formataCPF(cpf)) == false){
+            if (this.modelPaciente.validaDvsCpf(cpf) == true &&
+                this.modelPaciente.verificaIdade(dataNasc[0], dataNasc[1], dataNasc[2]) >= 13) {
+                if (this.modelPaciente.verificaRegistro(this.modelPaciente.formataCPF(cpf)) == false) {
                     //Enviando dados tratados para Model
 
                     this.modelPaciente.nomePaciente = nome;
@@ -30,96 +30,120 @@ export default class ControllerPaciente{
                     //Cadastrando paciente
                     this.modelPaciente.cadastraPaciente();
                     this.viewCadastro.mensagemCadastro();
-                }else{
+                } else {
                     //Verificação final, se o paciente já está cadastrado pelo CPF
                     this.viewCadastro.mensagemErroValidacao(nome, cpf, this.modelPaciente.formataDataNasc(dataNasc[0], dataNasc[1], dataNasc[2]));
                     //Mensagem de erro para CPF existente.
                     this.viewCadastro.mensagemErroCpfExistente();
                 }
-            }else{
+            } else {
                 //Caso retorne algum erro de validação, será verificado e retornado a mensagem correta.
                 this.viewCadastro.mensagemErroValidacao(nome, cpf, this.modelPaciente.formataDataNasc(dataNasc[0], dataNasc[1], dataNasc[2]));
 
-                if(this.modelPaciente.validaDvsCpf(cpf) == false){
+                if (this.modelPaciente.validaDvsCpf(cpf) == false) {
                     this.viewCadastro.mensagemErroCpfInvalido();
                 }
 
-                if(this.modelPaciente.verificaIdade(dataNasc[0], dataNasc[1], dataNasc[2]) < 13){
+                if (this.modelPaciente.verificaIdade(dataNasc[0], dataNasc[1], dataNasc[2]) < 13) {
                     this.viewCadastro.mensagemErroIdade();
                 }
 
             }
-        }else{
+        } else {
             //Caso retorne algum erro de input, será verificado e retornado a mensagem correta.
             this.viewCadastro.mensagemErroInput(nome, cpf);
-            if(this.validaNome(nome) == false){
+            if (this.validaNome(nome) == false) {
                 this.viewCadastro.mensagemErroNome();
             }
 
-            if(this.qtdCaracteresCPF(cpf) == false){
+            if (this.qtdCaracteresCPF(cpf) == false) {
                 this.viewCadastro.mensagemErroQtdCpf();
             }
-            
-            if(this.verificaDigitosCPF(cpf) == true){
+
+            if (this.verificaDigitosCPF(cpf) == true) {
                 this.viewCadastro.mensagemErroDigitosIguaisCpf();
             }
         }
     }
 
-    listaPacienteNome(){
+    listaPacienteNome() {
         const table = new Table({
-            head: ['CPF', 'Nome', 'Dt.Nasc', 'Idade']
+            head: ['CPF', 'Nome', 'Dt.Nasc']
         })
-        
-        let ordenacaoPaciente = this.modelPaciente.pacientes.slice().sort((a, b) => a.nome.localeCompare(b.nome))
-        
+
+        let ordenacaoPaciente = this.modelPaciente.ordenaPacientesNome()
+
         ordenacaoPaciente.forEach(paciente => {
-            table.push([paciente.cpf, paciente.nome, paciente.dataNasc, paciente.l]);
-          });
-          
-          // Exibe a tabela no console
-          console.log(table.toString());
+            table.push([paciente.cpf, paciente.nome, paciente.dataNasc]);
+        });
+
+        return console.log(table.toString());
     }
 
-    listaPacienteCPF(){
+    listaPacienteCPF() {
+        const table = new Table({
+            head: ['CPF', 'Nome', 'Dt.Nasc']
+        })
+
+        let ordenacaoPaciente = this.modelPaciente.ordenaPacientesNome();
+
+        ordenacaoPaciente.forEach(paciente => {
+            table.push([paciente.cpf, paciente.nome, paciente.dataNasc]);
+        });
+
+        return console.log(table.toString());
+    }
+
+    iniciaDelete(){
+        //Mensagem de entrada para captação do input
+        this.viewCadastro.mensagemDeletaPacienteInput();
+        let cpf = this.viewCadastro.recebeCpf();
+
+        let deletaPaciente = this.modelPaciente.deletaPaciente(cpf);
+        if(deletaPaciente === false){ //verifica se existe na base de dados
+            this.viewCadastro.mensagemNaoEncontrado();
+        }else{  
+            this.viewCadastro.mensagemDeletaSucesso();
+        }
+
     }
 
     //Tratamento dos dados obtidos
     //Validação do nome
-    validaNome(nomePessoa){
-        if(nomePessoa.length >= 5){
+    validaNome(nomePessoa) {
+        if (nomePessoa.length >= 5) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     //Tratamento para entrada do CPF
     //devido a constante necessidade de conversão para array, um método para agilizar e evitar duplicação de código
-    converteCPF(cpf){
+    converteCPF(cpf) {
         const numerosCPF = cpf.replace(/\D/g, "");
         const cpfArray = [...numerosCPF].map(Number);
         return cpfArray;
     }
 
     //Método para verificar se CPF tem a quantidade certa de caracteres
-    qtdCaracteresCPF(cpf){
-        if(this.converteCPF(cpf).length == 11){
+    qtdCaracteresCPF(cpf) {
+        if (this.converteCPF(cpf).length == 11) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     //Método para verificar se todos os digitos são iguais
-    verificaDigitosCPF(cpf){
+    verificaDigitosCPF(cpf) {
         const cpfArray = this.converteCPF(cpf);
 
         const caracteresIguais = cpfArray.every((caractere) => caractere === cpfArray[0]);
 
-        if(caracteresIguais){
+        if (caracteresIguais) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
