2e523d8cfb85eafe5b1d7b0194f966a484b314d8
Ferdzen <fernandaivp63@gmail.com>
2023-07-12 00:48:03 -0300

    Alteração do arquivo main da aplicação.

--- Desafio #1.2/App.js
+++ Desafio #1.2/App.js
@@ -1,6 +1,4 @@
 import ControllerMenus from './src/controller/ControllerMenus.js';
-import PromptSync from 'prompt-sync';
-const prompt = PromptSync({ sigint: true }); // Entrada de dados
 
 const cm = new ControllerMenus();
 
